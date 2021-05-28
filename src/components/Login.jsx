cb3b1ebb46a1fd8779fabbc7bb2dc04705b9c98a
JD Wilson <jaydee.wilson@gmail.com>
2021-05-28 10:49:38 +0200

    update login css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,6 +5,7 @@ import { loginUrl } from "./Spotify";
 export default function Login() {
     return (
         <div className='login'>
+            <h1> Spotify Profile</h1>
              <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
     )
